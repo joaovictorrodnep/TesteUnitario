b0a2dc48d10f7543de8ded0fcb85eb20a3990a49
joao victor <joaovictorrodnep@gmail.com>
2022-05-19 21:51:14 -0300

    all files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+/*
+4 Escreva um programa para ser usado na de portaria de um evento.
+a. Peça a idade. Menores de idade não são permitidos. Mensagem: “Negado. Menores de idade não são
+permitidos.”.
+b. Peça o tipo de convite. Os tipos de convite são comum, premium e luxo. Negar a entrada caso não
+seja nenhum destes. Mensagem: “Negado. Convite inválido.”.
+c. Peça o código do convite. Convites premium e luxo começam com “XL”. Convites comuns começam
+com “XT”. Caso o código não esteja nos padrões, negar a entrada. Mensagem: “Negado. Convite
+inválido.”.
+d. Caso todos os critérios sejam satisfeitos, exibir “Welcome :)”.
+*/
+
+
+
+
+fun main() {
+
+}
