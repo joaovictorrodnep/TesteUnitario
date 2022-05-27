61b7f27eb3803d56a770ac5fe9e125ad18f86f74
Rafael Menezes <rafaelchmenezes@protonmail.com>
2022-05-26 21:56:10 -0300

    any, unit e nothing --

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,11 +10,39 @@ inválido.”.
 d. Caso todos os critérios sejam satisfeitos, exibir “Welcome :)”.
 */
 
-//fun main() {
+fun main() {
 //    val message = getMessage(20, "comum", "XL213214")
 //    print(message)
-//
-//}
+//    address(state = "rj", city = "abc", number = null)
+    address("rj", "abc", "54654564", 20, 5)
+    printName()
+}
+
+fun address(city: String, state: String, zipcode: String = "", number: Int? = 0) {
+
+}
+
+fun <T> address(vararg parameters: T) {
+    var sum = 0
+    for (parameter in parameters) {
+        if (parameter is Int) {
+            sum += parameter
+        }
+        if (parameter is String) {
+            print("$parameter, ")
+        }
+    }
+    println()
+    println(sum)
+}
+
+fun printName() {
+    print("Rafael")
+}
+
+fun getName(): String {
+    return "Rafael"
+}
 
 fun isValidAge(age: Int): Boolean {
     return age in 18..200
